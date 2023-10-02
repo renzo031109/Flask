b1480e276f393d659db902f4df175a5eb44920a3
Renan Rivera <renzo031109@gmail.com>
2023-10-02 21:07:07 +0800

    Error site html

--- hello.py
+++ hello.py
@@ -8,8 +8,24 @@ app = Flask(__name__)
 def index():
     #return "<h1> Hello Renan </h1>"
     first_name = "Renan"
-    return render_template("index.html",first_name=first_name)
+    stuff = "<strong> Hello Guyss!!! </strong>"
+
+    family = ["Renan", "Uriel", "Renan", "Rain",50]
+
+    return render_template("index.html",
+                           first_name=first_name,
+                           stuff=stuff,
+                           family=family)
 
 @app.route('/user/<name>')
 def user(name):
     return render_template("user.html",user_name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"),500 
